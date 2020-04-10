659fc5c049983953831f314ea9a8c3c7014bcb74
Ilpo Lehtinen <feuer@feuerx.net>
2020-04-10 19:44:25 +0300

    tile defcppclass defclassed

--- tile.lisp
+++ tile.lisp
@@ -1,17 +1,16 @@
 (defpackage :qmapper.tile
   (:use :common-lisp
 	:cl-arrows
-	:qmapper.std))
+	:qmapper.std)
+  (:export :tile :tile-x :x :y :tile-y :tileset :tile-tileset :rotation :tile-rotation :sprite :tile-sprite))
 
 (in-package :qmapper.tile)
 
-(defcppclass Tile 
-  (public
-   (properties
-    (x 0)
-    (y 0)
-    (tileset 0) 
-    (rotation 0)
-    (sprite nil))))
+(defclass tile ()
+    ((x :accessor tile-x :initform 0)
+     (y :accessor tile-y :initform 0)
+     (tileset :accessor tile-tileset :initform 0) 
+     (rotation :accessor tile-rotation :initform 0)
+     (sprite :accessor tile-sprite :initform nil)))
 
 ;; (export-all :qmapper.tile)
